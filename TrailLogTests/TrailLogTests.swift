f5c94fc66238bd145255584d38c7563185abb8fc
Brian Maina <brianmaina.mach@gmail.com>
2025-05-24 17:00:18 +0400

    Fresh start with working Git repo

---
+++ TrailLogTests/TrailLogTests.swift
@@ -0,0 +1,16 @@
+//
+//  TrailLogTests.swift
+//  TrailLogTests
+//
+//  Created by Brian Maina on 5/24/25.
+//
+
+import Testing
+
+struct TrailLogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
